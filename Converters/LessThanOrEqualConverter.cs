5620624f5f338a6e5046e456bfe9f1e7105ff7ac
Stefan Cicka <cicka.stefan@gmail.com>
2024-08-01 17:03:33 +0200

    Changing design of UI in MagacinView

---
+++ Converters/LessThanOrEqualConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PetkusApplication.Converters
+{
+    public class LessThanOrEqualConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length == 2 && values[0] is int kolicina && values[1] is int minKolicina)
+            {
+                return kolicina <= minKolicina;
+            }
+            return false;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
